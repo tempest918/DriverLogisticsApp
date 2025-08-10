e2f4c988781e8c5225fe10e31e7d651fe6679273
Anthony Barnes <tonylbarnes@gmail.com>
2025-08-10 11:27:33 -0400

    Add validation and converters for improved user input
    - Introduced `IsNullConverter` for null checks in UI.
    - Enhanced `AddEditCompanyPage.xaml` with validation for multiple fields and error messages.
    - Added category picker validation in `AddExpensePage.xaml`.
    - Implemented validation for load number, freight rate, shipper, and consignee in `AddLoadPage.xaml`.
    - Improved user phone number and email validation in `ProfilePage.xaml`.

---
+++ DriverLogisticsApp/DriverLogisticsApp/Converters/IsNullConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace DriverLogisticsApp.Converters
+{
+
+    /// <summary>
+    /// checks if the value is null and returns true if it is, false otherwise
+    /// </summary>
+    public class IsNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Return true (visible) if the SelectedItem is null
+            return value == null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
